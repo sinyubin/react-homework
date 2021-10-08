6a880e61a726a4db6e64c0d0aa7bf4decaeb15bb
sinyubin <85838870+sinyubin@users.noreply.github.com>
2021-10-09 01:48:52 +0900

    리액트 심화 미완성

---
+++ src/elements/Input.js
@@ -0,0 +1,30 @@
+import React from "react";
+import styled from "styled-components";
+import { Text, Grid } from "./index";
+
+const Input = (props) => {
+  const {
+    label,
+    placeholder,
+    _onChange,
+    type,
+    multiLine,
+    value,
+    is_submit,
+    onsubmit,
+  } = props;
+
+  if (multiLine) {
+    return (
+      <Grid>
+        {label && <Text margin="0px">{label}</Text>}
+        <ElTextarea
+          row={10}
+          value={value}
+          placeholder={placeholder}
+          onChange={_onChange}
+        ></ElTextarea>
+      </Grid>
+    );
+  }
+};
